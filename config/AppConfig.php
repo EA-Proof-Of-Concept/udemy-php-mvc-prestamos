d9e8471467e2db8f641aec0c92d406e38c980380
Eric A. Rodríguez S <eazicomservicios@gmail.com>
2023-12-01 18:36:39 -0600

    Configuración de la aplicación.

---
+++ config/AppConfig.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 
+ */
+
+/** Establece la URL base de la aplicación. */
+define('URL', 'http://localhost:81/udemy-php-mvc-prestamos');
+
+define('TITULO', 'Sistema de Préstamos');
+
+define('MONEDA', '$');
+
+date_default_timezone_set('America/Mexico_City');
